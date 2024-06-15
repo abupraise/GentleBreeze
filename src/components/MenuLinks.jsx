e8df5be54b87e184294a97e19b19353f41a29745
abupraise <praiseabu2000@gmail.com>
2024-06-15 14:28:19 +0100

    Updating snippet

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const MenuLinks = () => {
   return (
     <div className="flex flex-col items-center lg:ml-16 w-full lg:w-64">
-      <h3 className="text-black font-signika font-semibold text-2xl mb-2">Menu</h3>
+      <h3 className="text-black font-signika font-semibold text-2xl mb-2">
+        Menu
+      </h3>
       <ul className="space-y-2">
-        <li className="text-black opacity-40 font-signika text-lg">Home</li>
-        <li className="text-black opacity-40 font-signika text-lg">About</li>
-        <li className="text-black opacity-40 font-signika text-lg">Trainers</li>
-        <li className="text-black opacity-40 font-signika text-lg">Plans</li>
-        <li className="text-black opacity-40 font-signika text-lg">Reviews</li>
+      <Link to="home" spy={true} smooth={true} duration={500}>
+        <li className="text-black opacity-40 font-signika text-lg">
+          {" "}
+            Home
+        </li>
+        </Link>
+        <Link to="about" spy={true} smooth={true} duration={500}>
+        <li className="text-black opacity-40 font-signika text-lg">
+            About
+        </li>
+        </Link>
+        <Link to="trainers" spy={true} smooth={true} duration={500}>
+        <li className="text-black opacity-40 font-signika text-lg">
+            Trainers
+        </li>
+        </Link>
+        <Link to="trainers" spy={true} smooth={true} duration={500}>
+        <li className="text-black opacity-40 font-signika text-lg">
+            Plans
+        </li>
+        </Link>
+        <Link to="trainers" spy={true} smooth={true} duration={500}>
+        <li className="text-black opacity-40 font-signika text-lg">
+            Reviews
+        </li>
+        </Link>
       </ul>
     </div>
   );
