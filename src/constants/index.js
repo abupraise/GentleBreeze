ffa2f2b5da046d7e6b6e49a5babc1ce47101f9e8
abupraise <praiseabu2000@gmail.com>
2024-07-06 16:36:37 +0100

    Updating Navbar

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "trainers",
+      title: "Trainers",
+    },
+    {
+      id: "plans",
+      title: "Plans",
+    },
+    {
+      id: "reviews",
+      title: "Reviews",
+    },
+  ];
+  
