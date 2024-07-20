892c44492325e943523e162a8c08db28043b507b
abupraise <praiseabu2000@gmail.com>
2024-07-20 19:08:44 +0100

    Updating contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,13 @@ import Dumbell from "/assets/images/dumbell.jpg";
 const Contact = () => {
   return (
     <div
-      className="relative flex flex-col items-center w-full py-16 lg:py-24 -mx-4 lg:-mx-8"
+      className="relative flex flex-col items-center w-full py-16 lg:py-24"
       style={{
         backgroundImage: `url(${Dumbell})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        width: "100vw",
+        marginLeft: "calc(-50vw + 50%)",
       }}
     >
       <div className="absolute inset-0 bg-black opacity-65"></div>
@@ -17,7 +19,7 @@ const Contact = () => {
           Call us Today
         </h2>
         <p className="text-white opacity-90 font-signika font-bold text-5xl uppercase mb-4">
-        +234 902 002 0403
+          +234 902 002 0403
         </p>
         <span className="text-white opacity-40 font-signika text-lg">
           Email: GENTLEBREEZE005@gmail.com
