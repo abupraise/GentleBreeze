8ffa2cf7e8ab910c0b2c8140df20e8cc1ee6b6d4
abupraise <praiseabu2000@gmail.com>
2024-09-30 17:52:01 +0100

    Updating snippet

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-scroll";
 const MenuLinks = () => {
   return (
     <div className="flex flex-col items-start lg:items-center w-full lg:w-64 lg:ml-16">
-      <h3 className="text-black font-signika font-semibold text-2xl mb-2 pl-4 lg:pl-0">
+      <h3 className="text-black font-signika font-semibold text-2xl mb-2 lg:pl-0">
         Menu
       </h3>
-      <ul className="space-y-2 text-left pl-4 lg:pl-0">
+      <ul className="space-y-2 text-left lg:pl-0">
         <Link to="home" spy={true} smooth={true} duration={500}>
           <li className="text-black opacity-40 font-signika text-lg hover:text-red-500 hover:opacity-100 cursor-pointer">
             Home
--- src/components/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks.jsx
@@ -5,10 +5,10 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 const SocialMediaLinks = () => {
   return (
     <div className="flex flex-col items-start lg:items-center w-full lg:w-64 lg:ml-16">
-      <h3 className="text-black font-signika font-semibold text-2xl mb-2 pl-4 lg:pl-0">
+      <h3 className="text-black font-signika font-semibold text-2xl mb-2 lg:pl-0">
         Follow Us On
       </h3>
-      <ul className="space-y-4 pl-4 lg:pl-0">
+      <ul className="space-y-4 lg:pl-0">
         {/* <li className="flex items-center space-x-4">
           <FontAwesomeIcon icon={faYoutube} className="w-6 h-6 opacity-40 text-black" />
           <span className="text-black opacity-40 font-signika text-lg">YouTube</span>
