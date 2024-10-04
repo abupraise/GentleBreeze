b92713b5fa39747528b95c8a12c022e6618e5149
abupraise <praiseabu2000@gmail.com>
2024-10-04 14:36:41 +0100

    Updating snippet

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Set the server to run on port 3000
+    port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true,
+  }
 });
